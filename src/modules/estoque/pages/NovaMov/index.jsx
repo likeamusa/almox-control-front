b1d4d1742e5ff07996d40792c527dfb4d3381672
Ítalo Kairon <italocentrismo@example.com>
2023-08-01 21:40:23 -0300

    feat: add entrada forms

--- src/modules/estoque/pages/NovaMov/index.jsx
+++ src/modules/estoque/pages/NovaMov/index.jsx
@@ -61,45 +61,374 @@ export default function NovaMov() {
                     <option value="nf">Nota Fiscal</option>
 
                     {/* inbound */}
-                    <option value="inbound">In-Bound</option>
+                    <option value="inbound">InBound</option>
+
+                    {/* outbound */}
+                    <option value="outbound">OutBound</option>
 
                     {/* outboud*/}
-                    <option value="outbound">Out-Bound</option>
+                    <option value="transferencia">Transferencia</option>
+
+                    {/* Inventario */}
+                    <option value="inventario">Inventario</option>
 
                 </select>
 
             </div>
-            {/* nf */}
+
+            {/* NOTA FISCAL */}
             {tipoMov === "nf" && (
-                <div className="flex flex-col items-center justify-center">
-                    <label className="text-gray-500 font-bold text-xl">Nota Fiscal</label>
-                    <input
-                        className="border border-gray-300 rounded-md text-gray-500 h-8 w-48"
-                    />
+                <div className="flex flex-col justify-center gap-4 align-middle">
+
+                    <form
+                        className="border border-gray-300 rounded-md p-4 flex flex-row gap-4 align-middle"
+                        onClick={(e) => e.preventDefault()}
+                    >
+
+                        {/* fornecedor */}
+                        <input
+                            placeholder="Fornecedor"
+                            className="border border-gray-300 rounded-md text-gray-500 h-8 w-48 p-4"
+                        />
+
+                        {/* Nota Fiscal */}
+                        <input
+                            placeholder="Nota Fiscal"
+                            className="border border-gray-300 rounded-md text-gray-500 h-8 w-48 p-4"
+                        />
+
+                        <input
+                            placeholder="Data"
+                            className="border border-gray-300 rounded-md text-gray-500 h-8 w-48 p-4"
+                        />
+
+                        {/* n pedido */}
+                        <input
+                            placeholder="Nº Pedido"
+                            className="border border-gray-300 rounded-md text-gray-500 h-8 w-48 p-4"
+                        />
+
+                        {/* botao confirmar / editar */}
+                        <button
+                            className="bg-blue-500 hover:bg-blue-600 text-white h-8 w-48 rounded-md m-auto"
+                        >Confirmar</button>
+
+                        {/* botao cancelar */}
+                        <button
+                            className="bg-gray-500 hover:bg-gray-600 text-white h-8 w-48 rounded-md m-auto"
+                        >Cancelar</button>
+
+                    </form>
+
+
+                    <div
+                        className="border border-gray-300 rounded-md p-4 flex flex-col align-middle"
+                    >
+                        <form
+                            className="flex flex-row gap-4 align-middle justify-evenly"
+                        >
+
+                            {/* material */}
+                            <input
+                                placeholder="Material"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-[28rem] p-4"
+                            />
+
+                            {/* quantidade */}
+                            <input
+                                placeholder="Quantidade"
+                                type="number"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-32 p-4"
+                            />
+
+                            {/* valor */}
+                            <input
+                                placeholder="Valor"
+                                type="number"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-32 p-4"
+                            />
+
+                            {/* Botao adicionar */}
+                            <button
+                                className="bg-blue-500 hover:bg-blue-600 text-white h-8 w-48 rounded-md m-auto"
+                            >Adicionar</button>
+
+                        
+                        </form>
+                    </div>
+
+
                 </div>
             )}
 
-            {/* inbound */}
+            {/* INBOUND */}
             {tipoMov === "inbound" && (
-                <div className="flex flex-col items-center justify-center">
-                    <label className="text-gray-500 font-bold text-xl">In-Bound</label>
-                    <input
-                        className="border border-gray-300 rounded-md text-gray-500 h-8 w-48"
-                    />
+                <div className="flex flex-col justify-center gap-4 align-middle">
+
+                    <form
+                        className="border border-gray-300 rounded-md p-4 flex flex-row gap-4 align-middle"
+                        onClick={(e) => e.preventDefault()}
+                    >
+
+                        {/* numero do pedido de transferencia */}
+                        <input
+                            placeholder="Nº Pedido de Transferencia"
+                            className="border border-gray-300 rounded-md text-gray-500 h-8 w-48 p-4"
+                        />
+
+                      
+                        {/* botao confirmar / editar */}
+                        <button
+                            className="bg-blue-500 hover:bg-blue-600 text-white h-8 w-48 rounded-md m-auto"
+                        >Confirmar</button>
+
+                        {/* botao cancelar */}
+                        <button
+                            className="bg-gray-500 hover:bg-gray-600 text-white h-8 w-48 rounded-md m-auto"
+                        >Cancelar</button>
+
+                    </form>
+
+
+                    <div
+                        className="border border-gray-300 rounded-md p-4 flex flex-col align-middle"
+                    >
+                        <form
+                            className="flex flex-row gap-4 align-middle justify-evenly"
+                        >
+
+                            {/* material */}
+                            <input
+                                placeholder="Material"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-[28rem] p-4"
+                            />
+
+                            {/* quantidade */}
+                            <input
+                                placeholder="Quantidade"
+                                type="number"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-32 p-4"
+                            />
+
+                            {/* valor */}
+                            <input
+                                placeholder="Valor"
+                                type="number"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-32 p-4"
+                            />
+
+                            {/* Botao adicionar */}
+                            <button
+                                className="bg-blue-500 hover:bg-blue-600 text-white h-8 w-48 rounded-md m-auto"
+                            >Adicionar</button>
+
+
+                        </form>
+                    </div>
+
+
                 </div>
             )}
 
-            {/* outbound */}
+            {/* OUTBOUND */}
             {tipoMov === "outbound" && (
-                <div className="flex flex-col items-center justify-center">
-                    <label className="text-gray-500 font-bold text-xl">Out-Bound</label>
+                <div className="flex flex-col justify-center gap-4 align-middle">
+
+                <form
+                    className="border border-gray-300 rounded-md p-4 flex flex-row gap-4 align-middle"
+                    onClick={(e) => e.preventDefault()}
+                >
+
+                    {/* numero do pedido de transferencia */}
                     <input
-                        className="border border-gray-300 rounded-md text-gray-500 h-8 w-48"
+                        placeholder="Nº Pedido de Transferencia"
+                        className="border border-gray-300 rounded-md text-gray-500 h-8 w-48 p-4"
                     />
+
+                  
+                    {/* botao confirmar / editar */}
+                    <button
+                        className="bg-blue-500 hover:bg-blue-600 text-white h-8 w-48 rounded-md m-auto"
+                    >Confirmar</button>
+
+                    {/* botao cancelar */}
+                    <button
+                        className="bg-gray-500 hover:bg-gray-600 text-white h-8 w-48 rounded-md m-auto"
+                    >Cancelar</button>
+
+                </form>
+
+
+                <div
+                    className="border border-gray-300 rounded-md p-4 flex flex-col align-middle"
+                >
+                    <form
+                        className="flex flex-row gap-4 align-middle justify-evenly"
+                    >
+
+                        {/* material */}
+                        <input
+                            placeholder="Material"
+                            className="border border-gray-300 rounded-md text-gray-500 h-8 w-[28rem] p-4"
+                        />
+
+                        {/* quantidade */}
+                        <input
+                            placeholder="Quantidade"
+                            type="number"
+                            className="border border-gray-300 rounded-md text-gray-500 h-8 w-32 p-4"
+                        />
+
+                        {/* valor */}
+                        <input
+                            placeholder="Valor"
+                            type="number"
+                            className="border border-gray-300 rounded-md text-gray-500 h-8 w-32 p-4"
+                        />
+
+                        {/* Botao adicionar */}
+                        <button
+                            className="bg-blue-500 hover:bg-blue-600 text-white h-8 w-48 rounded-md m-auto"
+                        >Adicionar</button>
+
+
+                    </form>
+                </div>
+
+
+            </div>
+            )}
+
+            {/* TRANSFERENCIA */}
+            {tipoMov === "transferencia" && (
+                <div className="flex flex-col justify-center gap-4 align-middle">
+
+                    <form
+                        className="border border-gray-300 rounded-md p-4 flex flex-row gap-4 align-middle"
+                        onClick={(e) => e.preventDefault()}
+                    >
+
+                        {/* origem */}
+                        <select
+                        className="border border-gray-300 rounded-md text-gray-500 h-8 w-48"
+                        >
+                            <option value="">Selecione origem</option>
+                            <option value="pl">Portelandia</option>
+                            <option value="rv">Rio-Verde</option>
+                        </select>
+
+                        {/* destino */}
+                        <select
+                        className="border border-gray-300 rounded-md text-gray-500 h-8 w-48"
+                        >
+                            <option value="">Selecione destino</option>
+                            <option value="pl">Portelandia</option>
+                            <option value="rv">Rio-Verde</option>
+                        </select>
+                      
+                        {/* botao confirmar / editar */}
+                        <button
+                            className="bg-blue-500 hover:bg-blue-600 text-white h-8 w-48 rounded-md m-auto"
+                        >Confirmar</button>
+
+                        {/* botao cancelar */}
+                        <button
+                            className="bg-gray-500 hover:bg-gray-600 text-white h-8 w-48 rounded-md m-auto"
+                        >Cancelar</button>
+
+                    </form>
+
+
+                    <div
+                        className="border border-gray-300 rounded-md p-4 flex flex-col align-middle"
+                    >
+                        <form
+                            className="flex flex-row gap-4 align-middle justify-evenly"
+                        >
+
+                            {/* material */}
+                            <input
+                                placeholder="Material"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-[28rem] p-4"
+                            />
+
+                            {/* quantidade */}
+                            <input
+                                placeholder="Quantidade"
+                                type="number"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-32 p-4"
+                            />
+
+                            {/* valor */}
+                            <input
+                                placeholder="Valor"
+                                type="number"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-32 p-4"
+                            />
+
+                            {/* Botao adicionar */}
+                            <button
+                                className="bg-blue-500 hover:bg-blue-600 text-white h-8 w-48 rounded-md m-auto"
+                            >Adicionar</button>
+
+
+                        </form>
+                    </div>
+
+
+                </div>
+
+            )}
+
+            {/* inventario */}
+            {tipoMov === "inventario" && (
+                <div className="flex flex-col justify-center gap-4 align-middle">
+
+
+                    <div
+                        className="border border-gray-300 rounded-md p-4 flex flex-col align-middle"
+                    >
+                        <form
+                            className="flex flex-row gap-4 align-middle justify-evenly"
+                        >
+
+                            {/* material */}
+                            <input
+                                placeholder="Material"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-[28rem] p-4"
+                            />
+
+                            {/* quantidade */}
+                            <input
+                                placeholder="Quantidade"
+                                type="number"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-32 p-4"
+                            />
+
+                            {/* valor */}
+                            <input
+                                placeholder="Valor"
+                                type="number"
+                                className="border border-gray-300 rounded-md text-gray-500 h-8 w-32 p-4"
+                            />
+
+                            {/* Botao adicionar */}
+                            <button
+                                className="bg-blue-500 hover:bg-blue-600 text-white h-8 w-48 rounded-md m-auto"
+                            >Adicionar</button>
+
+
+                        </form>
+                    </div>
+
+
                 </div>
             )}
 
-            <NovMovTable data={data} />
+            {/* TABELA */}
+            {tipoMov !== "" && (
+                <NovMovTable data={data} />
+            )}
 
         </Container>
     )
