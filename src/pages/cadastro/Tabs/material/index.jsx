4d7e2d45bcd7a29383efd5132b318cf97b93cb49
musa <italocentrismo@gmail.com>
2023-12-19 10:18:16 -0300

    feat: export materiais

--- src/pages/cadastro/Tabs/material/index.jsx
+++ src/pages/cadastro/Tabs/material/index.jsx
@@ -3,6 +3,7 @@ import TableComponent from "../../table";
 import ModalComponent from "../../modal";
 import { useSelector, useDispatch } from "react-redux";
 import { updateApp, saveMaterial } from "../../../../store/modules/app/actions";
+import jsonToExcel from "../../../../utils/jsonToExcel";
 
 
 const Material = () => {
@@ -18,30 +19,31 @@ const Material = () => {
 
     const setComponent = (component, value) => {
         dispatch(updateApp({
-            cadastro: {...cadastro, components: {...components, [component]: value}}}
+            cadastro: { ...cadastro, components: { ...components, [component]: value } }
+        }
         ))
     }
 
     const handleInputChange = (e) => {
         const { name, value } = e.target
-        setComponent("cadastroModalData", {...cadastroModalData, [name]: value})
+        setComponent("cadastroModalData", { ...cadastroModalData, [name]: value })
         console.log(cadastroModalData)
     }
 
     const handleSave = () => {
         dispatch(saveMaterial(cadastroModalData))
     }
-    
+
     return (
         <>
 
-            <ModalComponent 
-            onClickProp={e => {
-                handleSave()
-                setComponent("cadastroModal", false)
-            }} 
-            onInputChange={handleInputChange}
-            cadastroName={'material'} 
+            <ModalComponent
+                onClickProp={e => {
+                    handleSave()
+                    setComponent("cadastroModal", false)
+                }}
+                onInputChange={handleInputChange}
+                cadastroName={'material'}
             />
 
 
@@ -49,17 +51,26 @@ const Material = () => {
             <Barra>
                 <div></div>
                 <a
-                style={{cursor: "pointer"}}
-                onClick={() => setComponent("cadastroModal", true)}
+                    style={{ cursor: "pointer" }}
+                    onClick={() => setComponent("cadastroModal", true)}
                 >Adicionar
                 </a>
+
+                {/*Exportar*/}
+                <a
+                    style={{ cursor: "pointer" }}
+                    onClick={() => jsonToExcel(cadastro.cadastros.materials, "material")}
+                >Exportar
+                </a>
+
+
             </Barra>
 
-            <TableComponent 
-            cadastroName={'material'} 
-            onEdit={e => {
-                console.log(e)
-            }}
+            <TableComponent
+                cadastroName={'material'}
+                onEdit={e => {
+                    console.log(e)
+                }}
             />
         </>
     )
