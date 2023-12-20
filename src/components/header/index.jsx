c6c9fac29441006d449c3998f086e35c640a5970
Ítalo Kairon <italocentrismo@example.com>
2023-12-19 21:09:27 -0300

    logo alterada

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -23,7 +23,7 @@ const Header = () => {
                 cursor: 'pointer'
             }}
             >
-                Almox-Control
+                Almoxarifado Progen
             </h4>
             </a>
 
